24842e962df4670ebd60326fd0d331958c1d8079
LeeQianXi <qianxilee232@163.com>
2026-01-15 22:15:39 +0800

    fix

--- frontend/shared/vite.config.js
+++ frontend/shared/vite.config.js
@@ -1,6 +1,6 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,4 +20,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
